7f50d3ab0753c01c8b5653b1afbac5241f0e3d03
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-06 10:54:18 +0900

    Add event file

---
+++ qa-auto-generate-tag.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once QA_PLUGIN_DIR.'q2a-auto-generate-tag/qa-tag-select.php';
+
+class qa_auto_generate_tag
+{
+
+	function process_event($event, $userid, $handle, $cookieid, $params)
+	{
+
+		if (qa_using_tags() && $event === 'q_post') {
+			error_log($params['name']);
+			$tagsel = new qa_tag_select();
+			$tags = $tagsel->get_tags($params['categoryid'], $params['title'], $params['content']);
+
+			$oldquestion = $this->get_oldquestion($params['postid']);
+			qa_question_set_content($oldquestion, $oldquestion['title'],
+			 $oldquestion['content'], $oldquestion['format'],
+			 $oldquestion['text'], qa_tags_to_tagstring($tags),
+			 $oldquestion['notify'], $userid, $handle, $cookieid, $null, null, false, false);
+		}
+	}
+
+	private function get_oldquestion($postid = null)
+	{
+		$post = qa_db_single_select(qa_db_full_post_selectspec(null, $postid));
+		return $post;
+	}
+}
