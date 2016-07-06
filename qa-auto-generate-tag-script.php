f1c72bc783262cd33c9bf732c5977b9765c629c6
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-06 17:05:19 +0900

    Modify script

--- qa-auto-generate-tag-script.php
+++ qa-auto-generate-tag-script.php
@@ -8,13 +8,13 @@ error_log('-----------------------------------');
 error_log('auto generate tag start');
 require_once QA_PLUGIN_DIR.'q2a-auto-generate-tag/qa-tag-select.php';
 require_once QA_PLUGIN_DIR.'q2a-auto-generate-tag/agt-db-client.php';
-$start = microtime(true);
+// $start = microtime(true);
 if (qa_using_tags()) {
 	$count = agt_db_client::update_all_question_tags();
-	error_log('処理件数: ' . $count);
+	// error_log('処理件数: ' . $count);
 }
-$end = microtime(true);
-error_log("処理時間：" . ($end - $start) . "秒");
+// $end = microtime(true);
+// error_log("処理時間：" . ($end - $start) . "秒");
 error_log('auto generate tag finished');
 error_log('-----------------------------------');
 
