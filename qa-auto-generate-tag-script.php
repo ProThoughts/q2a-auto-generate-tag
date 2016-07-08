bd40a475f21960e46a0b74995b1c585ef102c0c0
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-08 11:28:12 +0900

    Modify script for conditions test

--- qa-auto-generate-tag-script.php
+++ qa-auto-generate-tag-script.php
@@ -8,14 +8,28 @@ error_log('-----------------------------------');
 error_log('auto generate tag start');
 require_once QA_PLUGIN_DIR.'q2a-auto-generate-tag/qa-tag-select.php';
 require_once QA_PLUGIN_DIR.'q2a-auto-generate-tag/agt-db-client.php';
-// $start = microtime(true);
+$start = microtime(true);
 if (qa_using_tags()) {
 	$tagsel = new qa_tag_select();
+	// $conditions = $tagsel->get_conditions();
+	// foreach ($conditions as $cond) {
+	// 	foreach ($cond as $key => $value) {
+	// 		if (is_array($value)) {
+	// 			echo $key . " : ";
+	// 			foreach ($value as $val) {
+	// 				echo $val . ",";
+	// 			}
+	// 			echo "\n";
+	// 		} else {
+	// 			echo $key . ' : ' . $value . "\n";
+	// 		}
+	// 	}
+	// }
 	$count = agt_db_client::update_all_question_tags();
 	error_log('処理件数: ' . $count);
 }
-// $end = microtime(true);
-// error_log("処理時間：" . ($end - $start) . "秒");
+$end = microtime(true);
+error_log("処理時間：" . ($end - $start) . "秒");
 error_log('auto generate tag finished');
 error_log('-----------------------------------');
 
